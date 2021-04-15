7b7b29c0f9fa1adc75de50fc96aeee0d36fc980b
Adam Wali <adam.wali@square63.org>
2021-04-15 10:01:51 +0500

    Fixed same token being generated everytime for user login

--- app/lib/authenticate_user.rb
+++ app/lib/authenticate_user.rb
@@ -1,8 +1,6 @@
 class AuthenticateUser
   attr_accessor :email, :password
 
-  TOKEN_EXPIRATION = 24.hours.from_now.freeze
-
   def initialize(params)
     @email = params[:email]
     @password = params[:password]
@@ -11,7 +9,7 @@ class AuthenticateUser
   def call
     raise ExceptionHandler::AuthenticationError, 'Invalid Credentials' if user.blank?
 
-    JsonWebToken.encode({ user_id: user.id }, TOKEN_EXPIRATION)
+    JsonWebToken.encode({ user_id: user.id }, 24.hours.from_now)
   end
 
   def user
