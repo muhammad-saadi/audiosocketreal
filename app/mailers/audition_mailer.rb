b915b483b6459aa1314f3f19103ea2e6b399cd53
Adam Wali <adam.wali@square63.org>
2021-04-20 15:46:45 +0500

    Resolved html parsing issue in mails

--- app/mailers/audition_mailer.rb
+++ app/mailers/audition_mailer.rb
@@ -1,9 +1,9 @@
 class AuditionMailer < ApplicationMailer
   def response_mail(email, content)
-    mail(to: email, subject: 'Audio socket audition response', body: content)
+    mail(content_type: "text/html", to: email, subject: 'Audio socket audition response', body: content)
   end
 
   def assignee_mail(email, id)
-    mail(to: email, subject: 'Audio socket notification', body: "You have assigned audition##{id}")
+    mail(content_type: "text/html", to: email, subject: 'Audio socket notification', body: "You have assigned audition##{id}")
   end
 end
