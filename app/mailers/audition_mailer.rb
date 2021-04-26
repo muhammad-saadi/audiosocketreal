913ffb5b969aee6c6578afa7f12ec6424885a5ee
Adam Wali <adam.wali@square63.org>
2021-04-26 15:16:25 +0500

    Added styled template to update status mail

--- app/mailers/audition_mailer.rb
+++ app/mailers/audition_mailer.rb
@@ -1,13 +1,13 @@
 class AuditionMailer < ApplicationMailer
   def response_mail(id, content)
     @audition = Audition.find(id)
+    @content = content
     mail(
       content_type: "text/html",
       from: @audition.email_config[:from],
       to: @audition.email,
       cc: @audition.email_config[:cc],
-      subject: @audition.email_subject,
-      body: content
+      subject: @audition.email_subject
     )
   end
 
