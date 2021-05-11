a8eacf3e463bf83f8b184977429e8588e200b048
Adam Wali <adam.wali@square63.org>
2021-05-11 14:48:43 +0500

    Attached /v1 with each url in docs

--- app/controllers/concerns/api/v1/docs/users_doc.rb
+++ app/controllers/concerns/api/v1/docs/users_doc.rb
@@ -9,14 +9,14 @@ module Api::V1::Docs::UsersDoc
     end
 
     def_param_group :doc_accept_invitation do
-      api :PATCH, '/users/accept_invitation', 'Accept invitation by setting password'
+      api :PATCH, '/v1/users/accept_invitation', 'Accept invitation by setting password'
       param :token, String, desc: 'Encoded id of the user', required: true
       param :password, String, desc: 'Password to be set', required: true
       param :password_confirmation, String, desc: 'Confirmation of password', required: true
     end
 
     def_param_group :doc_authenticate_token do
-      api :PATCH, '/users/authenticate_token', 'Authenticated given token and send status of user'
+      api :PATCH, '/v1/users/authenticate_token', 'Authenticated given token and send status of user'
       param :token, String, desc: 'Encoded id of the user', required: true
     end
   end
