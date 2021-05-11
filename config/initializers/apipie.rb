6b4e1cac880cb9ab1b3e7a648cd8e9fd6df6374f
Adam Wali <adam.wali@square63.org>
2021-05-11 14:48:43 +0500

    Attached /v1 with each url in docs

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,6 +1,6 @@
 Apipie.configure do |config|
   config.app_name                = "ArtistPortalBackend"
-  config.api_base_url            = "/api/v1"
+  config.api_base_url            = "/api"
   config.doc_base_url            = "/documentation"
   config.translate = false
   # where is your API defined?
