41faba12a27fa8af142be9e709b1dfe73bfa1b65
Adam Wali <adam.wali@square63.org>
2021-05-21 16:37:58 +0500

    Updated doc and added validation on contact information

--- app/controllers/concerns/api/v1/docs/artist_profiles_doc.rb
+++ app/controllers/concerns/api/v1/docs/artist_profiles_doc.rb
@@ -12,8 +12,15 @@ module Api::V1::Docs::ArtistProfilesDoc
       param :bio, String, desc: 'Biography of the artist'
       param :sounds_like, String, desc: "How do artist's sounds looks alike?"
       param :key_facts, String, desc: 'Some key facts about artist'
-      param :contact, String, desc: 'Contact info of artist'
       param :social, Array, of: String, desc: 'Social media account links'
+      param :contact_information_attributes, Hash, desc: 'Contact Information', required: true do
+        param :name, String, desc: "Name for contact", required: true
+        param :street, String, desc: "Street number", required: true
+        param :postal_code, String, desc: "Postal code of area", required: true
+        param :city, String, desc: "City", required: true
+        param :state, String, desc: "State", required: true
+        param :country, String, desc: "Country", required: true
+      end
     end
 
     def_param_group :doc_show_profile do
--- app/models/contact_information.rb
+++ app/models/contact_information.rb
@@ -1,3 +1,5 @@
 class ContactInformation < ApplicationRecord
   belongs_to :artist_profile
+
+  validates :name, :street, :postal_code, :city, :state, :country, presence: true
 end
