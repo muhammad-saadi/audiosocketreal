09aa16d1ce30426424625e04c061927c2c03066e
Adam Wali <adam.wali@square63.org>
2021-05-27 17:09:17 +0500

    Updated serializer indentation issue

--- app/serializers/api/v1/artists_collaborator_serializer.rb
+++ app/serializers/api/v1/artists_collaborator_serializer.rb
@@ -4,10 +4,11 @@ class Api::V1::ArtistsCollaboratorSerializer < ActiveModel::Serializer
   belongs_to :artist, class_name: 'User'
 
   def meta
+    collaborator = object.collaborator
     {
-    password: object.collaborator.encrypted_password.present?,
-    agreements: object.collaborator.users_agreements && object.collaborator.users_agreements.joins(:agreement).where('agreement.agreement_type': [Agreement::TYPES[:exclusive], Agreement::TYPES[:non_exclusive]]).pluck(:status).all?('accepted'),
-    token: object.collaborator.encoded_id
+      password: collaborator.encrypted_password.present?,
+      agreements: collaborator.users_agreements && object.collaborator.users_agreements.joins(:agreement).where('agreement.agreement_type': [Agreement::TYPES[:exclusive], Agreement::TYPES[:non_exclusive]]).pluck(:status).all?('accepted'),
+      token: collaborator.encoded_id
     }
   end
 end
