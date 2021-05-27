98ef6e93f4a23e4dbb68839a9e99fbe31c7df2ec
Adam Wali <adam.wali@square63.org>
2021-05-27 18:12:55 +0500

    Moved united states string to constant

--- app/models/payment_information.rb
+++ app/models/payment_information.rb
@@ -4,9 +4,11 @@ class PaymentInformation < ApplicationRecord
   validates :payee_name, :bank_name, :routing, :account_number, presence: true
   validates :paypal_email, presence: true, unless: :united_states?, allow_blank: false
 
+  US = 'united states'.freeze
+
   private
 
   def united_states?
-    artist_profile.contact_information.country.downcase == 'united states'
+    artist_profile.contact_information.country.downcase == US
   end
 end
