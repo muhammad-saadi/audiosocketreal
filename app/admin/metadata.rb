7864dbc70f719076bbb08401a278e74d213c30c0
Adam Wali <adam.wali@square63.org>
2021-06-21 16:57:57 +0500

    Customize toolbar for metadata

--- app/admin/metadata.rb
+++ app/admin/metadata.rb
@@ -2,6 +2,22 @@ ActiveAdmin.register Metadata do
   actions :all, except: [:destroy, :new, :create]
   permit_params :content
 
+  index do
+    selectable_column
+    id_column
+    column :key
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :key
+      row :content do |metadata|
+        metadata.content.html_safe
+      end
+    end
+  end
+
   form do |f|
     f.inputs do
       f.input :key, input_html: { disabled: true }
--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,6 +1,10 @@
 CKEDITOR.editorConfig = function (config) {
   config.toolbar_mini = [
-    ["Bold", "Italic", "Underline", "Strike", "-", "Subscript", "Superscript"],
+    { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align'], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
+    { name: 'styles', items: [ 'Font', 'FontSize' ] },
+    { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+    { name: 'insert', items: ['Table', 'HorizontalRule', 'SpecialChar' ] }
   ];
   config.toolbar = "mini";
 }
