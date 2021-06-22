d1c8f33f09c81d1e9007b051be21e30ef029c545
Adam Wali <adam.wali@square63.org>
2021-06-22 17:19:55 +0500

    Made change column migration reversible

--- db/migrate/20210622114550_change_column_type_of_collaborator.rb
+++ db/migrate/20210622114550_change_column_type_of_collaborator.rb
@@ -1,5 +1,11 @@
 class ChangeColumnTypeOfCollaborator < ActiveRecord::Migration[6.1]
   def change
-    change_column :collaborator_profiles, :different_registered_name, :string
+    def self.up
+      change_column :collaborator_profiles, :different_registered_name, :string
+    end
+
+    def self.down
+      change_column :collaborator_profiles, :different_registered_name, :boolean
+    end
   end
 end
