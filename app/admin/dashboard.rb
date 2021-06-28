5cc088bd1120a342e2b0805b0c82415f12990409
Adam Wali <adam.wali@square63.org>
2021-06-28 18:01:47 +0500

    Updated dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,19 +2,42 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    columns do
-      column do
-        panel "Total Auditions" do
-          ul do
-            Audition.count
+    hr
+    div do
+      columns do
+        column do
+          panel "Total Artists" do
+            User.artist.count
+          end
+        end
+
+        column do
+          panel "Total Albums" do
+            Album.count
+          end
+        end
+
+        column do
+          panel "Total Track" do
+            Track.count
           end
         end
       end
+    end
+    hr
+    br
+    br
+    div do
+      columns do
+        column do
+          panel "Unclassified Tracks (#{Track.unclassified.count})" do
+            link_to 'Go to Tracks', admin_tracks_path(scope: 'unclassified')
+          end
+        end
 
-      column do
-        panel "Total Users" do
-          ul do
-            User.count
+        column do
+          panel "Pending Notes (#{Note.pending.count})" do
+            link_to 'Go to Notes', admin_notes_path(scope: 'pending')
           end
         end
       end
