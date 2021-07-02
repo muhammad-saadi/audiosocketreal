061d1f13b55184ce6ed43703d148bf7af47398c7
Adam Wali <adam.wali@square63.org>
2021-07-02 15:45:34 +0500

    Integrated rswag for API documentation

---
+++ spec/requests/api/albums_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'swagger_helper'
+
+describe 'Albums API' do
+  path '/albums/{id}' do
+    get 'Retrieves an album' do
+      tags 'Albums'
+      produces 'application/json'
+      parameter name: :id, in: :path, type: :string
+
+      response '200', 'Album found' do
+        schema type: :object,
+               properties: {
+                 id: { type: :integer },
+                 name: { type: :string },
+                 release_date: { type: :date }
+               },
+               required: %w[id name]
+
+        let(:id) { Album.create(name: 'XYZ', release_date: Date.today).id }
+        run_test!
+      end
+
+      response '404', 'album not found' do
+        let(:id) { 'invalid' }
+        run_test!
+      end
+    end
+  end
+end
