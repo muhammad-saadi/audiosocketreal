34c2bd9da371bc597142f772e85c4dd095cd25d0
Adam Wali <adam.wali@square63.org>
2021-07-14 10:08:20 +0500

    Added featured in filters

--- app/admin/filters.rb
+++ app/admin/filters.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Filter do
-  permit_params :name, :parent_filter_id, :max_levels_allowed
+  permit_params :name, :parent_filter_id, :max_levels_allowed, :featured
 end
--- db/migrate/20210713065024_create_filters.rb
+++ db/migrate/20210713065024_create_filters.rb
@@ -3,6 +3,7 @@ class CreateFilters < ActiveRecord::Migration[6.1]
     create_table :filters do |t|
       t.string :name
       t.integer :max_levels_allowed
+      t.boolean :featured
 
       t.references :parent_filter
       t.timestamps
