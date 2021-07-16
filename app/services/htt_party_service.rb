9058d63fbc0605813bd43806d38d18a8f7e63bf0
Adam Wali <adam.wali@square63.org>
2021-07-16 16:54:49 +0500

    HTTParty Service Removed

--- app/services/htt_party_service.rb
+++
@@ -1,3 +0,0 @@
-class HttPartyService
-  include HTTParty
-end
--- app/services/tax_id_service.rb
+++ app/services/tax_id_service.rb
@@ -1,6 +1,8 @@
 require 'open-uri'
 
 class TaxIdService
+  BASE_URL = "https://api.taxid.pro"
+
   def self.create_form(current_user)
     current_user = current_user
     key= ENV['TAXID_KEY']
@@ -10,12 +12,12 @@ class TaxIdService
 
     @options = { body: { key: key, thankYouUrl: thankYouUrl, formNumber: formNumber, reference: reference } }
 
-    HttPartyService.post("https://api.taxid.pro/formRequests", @options).parsed_response
+    HTTParty.post("#{BASE_URL}/formRequests", @options).parsed_response
   end
 
   def self.submit_form(params)
     form_token = params[:token]
-    pdf_link = HttPartyService.get("https://api.taxid.pro/forms/#{form_token}/pdf",{body: { key: ENV['TAXID_KEY'] }}).parsed_response
+    pdf_link = HTTParty.get("#{BASE_URL}/forms/#{form_token}/pdf",{body: { key: ENV['TAXID_KEY'] }}).parsed_response
     tax_information = TaxInformation.find_or_create_by(artist_profile_id: params[:reference])
     tax_information.tax_forms.attach(io: open(pdf_link['pdf']), filename: 'taxform.pdf')
   end
