015f97d60d44fa2b571acf9a8c93e140814e6bac
Adam Wali <adam.wali@square63.org>
2021-07-19 10:51:57 +0500

    Updated filters CRUD with allowed levels

--- app/admin/filters.rb
+++ app/admin/filters.rb
@@ -1,3 +1,75 @@
 ActiveAdmin.register Filter do
   permit_params :name, :parent_filter_id, :max_levels_allowed, :featured
+
+  controller do
+    def scoped_collection
+      end_of_association_chain.parent_filters
+    end
+
+    def show
+      @filter = Filter.find(params[:id])
+    end
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :featured
+    column :created_at
+    column :updated_at
+
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+      row :max_levels_allowed
+      row :featured
+      row :parent_filter
+      row :created_at
+      row :updated_at
+    end
+
+    if filter.max_levels_allowed.positive?
+      panel 'Sub-Filters' do
+        panel('', class: 'align-right') do
+          link_to 'Add new Sub-Filter', new_admin_filter_path(filter: { parent_filter_id: filter.id }), class: 'medium button'
+        end
+
+        table_for filter.sub_filters do
+          if filter.sub_filters.blank?
+            column 'No Records Found'
+          else
+            column :id
+            column :name
+            column :featured
+            column :actions do |filter|
+              link_to 'view', admin_filter_path(filter), class: 'small button'
+            end
+          end
+        end
+      end
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :parent_filter, as: :select, collection: [f.object.parent_filter], include_blank: false
+      f.input :name
+      f.input :featured
+      if f.object.parent_filter.blank?
+        f.input :max_levels_allowed
+      else
+        f.object.max_levels_allowed = f.object.parent_filter.max_levels_allowed - 1
+        f.input :max_levels_allowed, as: :hidden
+      end
+    end
+
+    f.actions do
+      f.action :submit
+      f.cancel_link(f.object.persisted? ? { action: 'show' } : admin_filter_path(f.object.parent_filter_id))
+    end
+  end
 end
--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,5 +1,5 @@
 class Filter < ApplicationRecord
-  validates :name, presence: true
+  validates :name, :max_levels_allowed, presence: true
 
   has_many :sub_filters, foreign_key: 'parent_filter_id', class_name: 'Filter', dependent: :destroy
   has_many :track_filters, dependent: :destroy
