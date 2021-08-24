fba9d6e1c955750c9109edb79eca27c147f3a741
Adam Wali <adam.wali@square63.org>
2021-08-24 18:23:45 +0500

    Updated track serializer

--- app/serializers/api/v1/track_serializer.rb
+++ app/serializers/api/v1/track_serializer.rb
@@ -1,9 +1,9 @@
 class Api::V1::TrackSerializer < BaseSerializer
   attributes :id, :title, :file, :status, :public_domain, :created_at, :lyrics, :explicit, :composer, :description,
-             :language, :instrumental, :key, :bpm, :admin_note, :filters
+             :language, :instrumental, :key, :bpm, :admin_note, :filters, :publishers, :artists_collaborators
 
   has_many :publishers, serializer: Api::V1::PublisherSerializer
-  has_many :artists_collaborators, serializer: Api::V1::ArtistsCollaboratorSerializer
+  has_many :artists_collaborators, serializer: Api::V1::CollaboratorSerializer
 
   def file
     object.file.presence && UrlHelpers.rails_blob_url(object.file)
