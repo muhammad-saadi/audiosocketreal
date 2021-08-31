bc1c8c20a914b0482f5766e0c76d6faff0d79c70
Adam Wali <adam.wali@square63.org>
2021-08-31 13:27:44 +0500

    Aims CRUD

--- app/controllers/api/v1/aims_api_controller.rb
+++ app/controllers/api/v1/aims_api_controller.rb
@@ -3,12 +3,10 @@ class Api::V1::AimsApiController < Api::BaseController
   skip_before_action :authorize_request, only: %i[track_response]
 
   def track_response
-    byebug
     track_client_id = params[:aims_api][:id_client]
     track = Track.find(track_client_id)
     track.aims_id = params[:aims_api][:id]
     track.aims_status = params[:aims_api][:status]
     track.save!
-    byebug
   end
 end
