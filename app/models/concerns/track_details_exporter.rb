b958235f2ca098b6029291ba2b5b7cc98d69e079
Adam Wali <adam.wali@square63.org>
2021-09-02 17:10:40 +0500

    Updated track details export

--- app/models/concerns/track_details_exporter.rb
+++ app/models/concerns/track_details_exporter.rb
@@ -5,43 +5,57 @@ module TrackDetailsExporter
     def track_detail_sheet
       io = StringIO.new
       xlsx = Xlsxtream::Workbook.new(io)
-      xlsx.write_worksheet 'Sheet1' do |sheet|
 
+      xlsx.write_worksheet 'Sheet1' do |sheet|
         sheet << ['SynchTank ID', 'Parent track', 'MP3 File', 'WAV File', 'AIFF File', 'Title', 'Performed By', 'Album', 'Composer', 'Publishers', 'Writers', 'Notes', 'Description', 'Lyrics', 'Language', 'Instrumental', 'Explicit', 'Vocals', 'Key', 'BPM', 'Tempo', 'Genres :: Comma Sep', 'Subgenres :: Comma Sep', 'Moods :: Comma Sep', 'Instruments :: Comma Sep', 'Subinstruments :: Comma Sep']
-        sheet << ['id', 'parent_id', 'mp3_filename', 'wav_filename', 'aiff_filename', 'title', 'performed_by', 'album', 'composer', 'publishers_csv', 'writers_csv', 'notes', 'description', 'lyrics', 'language', 'instrumental', 'explicit', 'vocals', 'musical_key', 'bpm', 'tempo', 'metadata_genres_csv', 'metadata_subgenres_csv', 'metadata_moods_csv', 'metadata_instruments_csv', 'metadata_subinstruments_csv']
+        sheet << %w[id parent_id mp3_filename wav_filename aiff_filename title performed_by album composer publishers_csv writers_csv notes description lyrics language instrumental explicit vocals musical_key bpm tempo metadata_genres_csv metadata_subgenres_csv metadata_moods_csv metadata_instruments_csv metadata_subinstruments_csv]
 
         all.each do |track|
-          mp3_filename = track.file.filename if track.file.content_type == "audio/mpeg"
-          wav_filename = track.file.filename if track.file.content_type == "audio/vnd.wave" || track.file.content_type == "audio/wave"
-          aiff_filename = track.file.filename if track.file.content_type == "audio/aiff" || track.file.content_type == "audio/x-aiff"
-          title = track.title
-          performed_by = track.album.user.full_name
-          album = track.album.name
-          composer = track.composer
-          publishers = track.publishers.pluck(:name)
-          writers = track.track_writers.map(&:collaborator_name)
-          notes = track.admin_note
-          description = track.description
-          lyrics = track.lyrics
-          language = track.language
-          instrumental = track.instrumental ? 1 : 0
-          explicit = track.explicit ? 1 : 0
-          vocals = track.filters.select { |filter| filter.parent_filter.name.downcase.include?('vocals') }.pluck(:name)
-          key = track.key
-          bpm = track.bpm
-          tempo = track.filters.select { |filter| filter.parent_filter.name.downcase.include?('tempos') }.pluck(:name)
-          genres = track.filters.select { |filter| filter.parent_filter.name.downcase.include?('genres') }.pluck(:name)
-          sub_genres = track.filters.select{ |filter| filter.parent_filter.name.in?(genres) }.pluck(:name)
-          moods = track.filters.select { |filter| filter.parent_filter.name.downcase.include?('moods') }.pluck(:name)
-          instruments = track.filters.select { |filter| filter.parent_filter.name.downcase.include?('instruments') }.pluck(:name)
-          sub_instruments = track.filters.select{ |filter| filter.parent_filter.name.in?(instruments) }.pluck(:name)
-
-          sheet << [nil, nil, mp3_filename, wav_filename, aiff_filename, title, performed_by, album, composer, publishers.join(','), writers.join(','), notes, description, lyrics, language, instrumental, explicit, vocals.join(','), key, bpm, tempo.join(','), genres.join(','), sub_genres.join(','), moods.join(','), instruments.join(','), sub_instruments.join(',')]
+          row = ['', '']
+          row << (track.mpeg? ? track.file.filename : '')
+          row << (track.wav? ? track.file.filename : '')
+          row << (track.aiff? ? track.file.filename : '')
+          row << track.title
+          row << track.album.user.full_name
+          row << track.album.name
+          row << track.composer
+          row << track.publishers.pluck(:name).join(', ')
+          row << track.track_writers.map(&:collaborator_name).join(', ')
+          row << track.admin_note
+          row << track.description
+          row << track.lyrics
+          row << track.language
+          row << (track.instrumental ? 1 : 0)
+          row << (track.explicit ? 1 : 0)
+          row << track.filters.select { |filter| filter.parent_filter.name.downcase.include?('vocals') }.pluck(:name).join(', ')
+          row << track.key
+          row << track.bpm
+          row << track.filters.select { |filter| filter.parent_filter.name.downcase.include?('tempos') }.pluck(:name).join(', ')
+          genres = track.filters.select { |filter| filter.parent_filter.name.downcase.include?('genres') }.pluck(:name).join(', ')
+          row << genres
+          row << track.filters.select { |filter| filter.parent_filter.name.in?(genres) }.pluck(:name).join(', ')
+          row << track.filters.select { |filter| filter.parent_filter.name.downcase.include?('moods') }.pluck(:name).join(', ')
+          instruments = track.filters.select { |filter| filter.parent_filter.name.downcase.include?('instruments') }.pluck(:name).join(', ')
+          row << instruments
+          row << track.filters.select { |filter| filter.parent_filter.name.in?(instruments) }.pluck(:name).join(', ')
+          sheet << row
         end
-
       end
+
       xlsx.close
       io.string
     end
+  end
+
+  def mpeg?
+    file.content_type == 'audio/mpeg'
+  end
+
+  def wav?
+    file.content_type == 'audio/vnd.wave' || file.content_type == 'audio/wave'
+  end
+
+  def aiff?
+    file.content_type == 'audio/aiff' || file.content_type == 'audio/x-aiff'
   end
 end
