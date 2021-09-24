90d2b13337704bf560720f07434e2877904963a1
Adam Wali <adam.wali@square63.org>
2021-09-24 17:37:15 +0500

    Refactored class method calling

--- app/services/omniauth_login.rb
+++ app/services/omniauth_login.rb
@@ -7,16 +7,16 @@ class OmniauthLogin
   end
 
   def self.google_url
-    OmniauthLogin.google_client.auth_code.authorize_url({
-                                                          scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
-                                                          redirect_uri: ENV['FRONTEND_GOOGLE_CALLBACK'],
-                                                          access_type: 'offline',
-                                                          prompt: 'consent'
-                                                        })
+    google_client.auth_code.authorize_url({
+                                            scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+                                            redirect_uri: ENV['FRONTEND_GOOGLE_CALLBACK'],
+                                            access_type: 'offline',
+                                            prompt: 'consent'
+                                          })
   end
 
   def self.google_callback_response(code)
-    access_token = OmniauthLogin.google_client.auth_code.get_token(code, redirect_uri: ENV['FRONTEND_GOOGLE_CALLBACK'])
+    access_token = google_client.auth_code.get_token(code, redirect_uri: ENV['FRONTEND_GOOGLE_CALLBACK'])
     response = HTTParty.get('https://www.googleapis.com/oauth2/v1/userinfo?alt=json', headers: { Authorization: "Bearer #{access_token.token}" }).parsed_response
     consumer = Consumer.find_or_initialize_by(email: response['email'])
 
