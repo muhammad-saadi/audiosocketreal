8e867857c54bec5ac76967baf01687b5fdb04738
Adam Wali <adam.wali@square63.org>
2021-09-24 17:28:43 +0500

    Added ability to login with google

--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -2,13 +2,15 @@ class Consumer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
 
   validates :first_name, :last_name, presence: true
 
   has_one :consumer_profile, dependent: :destroy
 
   accepts_nested_attributes_for :consumer_profile
+
+  attr_accessor :skip_password_validation
 
   def self.authenticate(email, password)
     user = Consumer.find_for_authentication(email: email)
@@ -17,5 +19,17 @@ class Consumer < ApplicationRecord
 
   def full_name
     [first_name, last_name].compact.join(' ')
+  end
+
+  def encoded_id
+    JsonWebToken.encode({ consumer_id: id })
+  end
+
+  private
+
+  def password_required?
+    return false if skip_password_validation
+
+    super
   end
 end
