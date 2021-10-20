aea6316bbd4e36b524a7c533bac162a0148eb818
Adam Wali <adam.wali@square63.org>
2021-10-20 19:51:22 +0500

    Refactored Ordering Methods

--- app/models/curated_playlist.rb
+++ app/models/curated_playlist.rb
@@ -16,22 +16,26 @@ class CuratedPlaylist < ApplicationRecord
   before_validation :order_limit, on: :update
   after_destroy :set_order
 
+  scope :delete_order, -> (order) { where("curated_playlists.order > ?", order) }
+  scope :below_order, ->(order, order_was) { where("curated_playlists.order >= ? and curated_playlists.order <= ?", order, order_was) }
+  scope :above_order, ->(order, order_was) { where("curated_playlists.order <= ? and curated_playlists.order >= ? ", order, order_was) }
+
   def create_order
     self.order = CuratedPlaylist.maximum('order').to_i + 1
   end
 
   def set_order
-    below_order = CuratedPlaylist.where("curated_playlists.order > ?", self.order)
-    CuratedPlaylist.decrement_counter(:order, below_order)
+    delete_order = CuratedPlaylist.delete_order(self.order)
+    CuratedPlaylist.decrement_counter(:order, delete_order)
   end
 
   def order_limit
     errors.add(:order, 'Order out of bounds') if order > CuratedPlaylist.maximum('order').to_i
     if order_was > order
-      below_order = CuratedPlaylist.where("curated_playlists.order >= ? and curated_playlists.order <= ?", order, order_was).without(self)
+      below_order = CuratedPlaylist.below_order(order, order_was).without(self)
       CuratedPlaylist.increment_counter(:order, below_order)
     elsif order_was < order
-      above_order = CuratedPlaylist.where("curated_playlists.order <= ? and curated_playlists.order >= ? ", order, order_was).without(self)
+      above_order = CuratedPlaylist.above_order(order, order_was).without(self)
       CuratedPlaylist.decrement_counter(:order, above_order)
     end
   end
