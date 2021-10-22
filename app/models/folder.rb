25ba708aa9545d16a6936a4f55591bf6a5214453
Adam Wali <adam.wali@square63.org>
2021-10-22 10:25:40 +0500

    Refactored Validation checks and removed extra migrations

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,52 +1,53 @@
 class Folder < ApplicationRecord
   belongs_to :consumer
-  belongs_to :parent_folder, class_name: "Folder", optional: true
-  has_many :sub_folders, class_name: "Folder", foreign_key: "parent_folder_id", dependent: :destroy
+  belongs_to :parent_folder, class_name: 'Folder', optional: true
+
   has_many :consumer_playlists, dependent: :destroy
+  has_many :sub_folders, class_name: 'Folder', foreign_key: 'parent_folder_id', dependent: :destroy
 
-  validate :set_folder, on: [:create, :update]
-  validate :level
+  validate :same_consumer
 
-  before_create :set_level
+  before_validation :set_level, :level_validation
 
-  parent_folders_sum = 0
+  MAX_LEVEL_ALLOWED = 1
 
-  def self.subfolder_hash
-    arr = {}
-    temp = arr
-    max_level = Consumer.first.folders.maximum('max_levels_allowed')
+  def self.sub_folder_hash(consumer)
+    eagerload_columns = {}
+    current_level = eagerload_columns
+    max_level = consumer.folders.maximum('level') + 1
+
     max_level.times.each do
-      temp[:sub_folders] = consumer_playlist_hash
-      temp = temp[:sub_folders]
+      current_level[:sub_folders] = consumer_playlist_hash
+      current_level = current_level[:sub_folders]
     end
-    arr
+
+    eagerload_columns
   end
 
   def self.consumer_playlist_hash
-    { consumer_playlists: [ banner_image_attachment: :blob, playlist_image_attachment: :blob, tracks: [:alternate_versions, filters: [:parent_filter, sub_filters: [sub_filters: :sub_filters]], file_attachment: :blob]] }
+    { consumer_playlists: [banner_image_attachment: :blob, playlist_image_attachment: :blob, tracks: [:alternate_versions, filters: [:parent_filter, sub_filters: [sub_filters: :sub_filters]], file_attachment: :blob]] }
   end
 
-  def self.eagerload_cols
+  def self.eagerload_cols(consumer)
     [
-      subfolder_hash,
+      sub_folder_hash(consumer),
       consumer_playlist_hash
     ]
   end
 
-  def set_folder
-    consumer.folders.find(parent_folder_id) if parent_folder_id.present?
+  def same_consumer
+    return if parent_folder_id.blank?
+
+    errors.add(:parent_folder, 'is invalid') if consumer.id != parent_folder&.consumer_id
   end
 
   def set_level
-    self.max_levels_allowed = check_parent_folder(self)
+    return if parent_folder.blank?
+
+    self.level = parent_folder.level + 1
   end
 
-  def level
-    errors.add(:parent_folder, 'Folder limit reached') if parent_folder.present? && parent_folder.max_levels_allowed > 0
-  end
-  
-  def check_parent_folder(folder, parent_folders_sum = 0)
-    return  parent_folders_sum if folder.parent_folder_id.blank?
-    check_parent_folder(Folder.find(folder.parent_folder_id), parent_folders_sum + 1)
+  def level_validation
+    errors.add(:parent_folder, 'Folder limit reached') if level > MAX_LEVEL_ALLOWED
   end
 end
--- db/migrate/20210914050908_add_deletable_to_folder.rb
+++
@@ -1,5 +0,0 @@
-class AddDeletableToFolder < ActiveRecord::Migration[6.1]
-  def change
-    add_column :folders, :deletable, :boolean, default: :true
-  end
-end
