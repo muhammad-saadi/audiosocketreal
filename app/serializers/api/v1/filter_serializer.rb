1152ad56b18424cd10fde04aa42a80d2825ab6cd
Adam Wali <adam.wali@square63.org>
2021-11-29 13:28:25 +0500

    Tracks duration and track counts for filters

--- app/serializers/api/v1/filter_serializer.rb
+++ app/serializers/api/v1/filter_serializer.rb
@@ -1,7 +1,11 @@
 class Api::V1::FilterSerializer < BaseSerializer
-  attributes :id, :name, :sub_filters
+  attributes :id, :name, :sub_filters, :track_count
 
   def sub_filters
     object.sub_filters&.map { |filter| Api::V1::FilterSerializer.new(filter) }
+  end
+
+  def track_count
+    object.tracks.size
   end
 end
