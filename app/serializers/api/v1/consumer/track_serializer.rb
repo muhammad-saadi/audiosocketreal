01769a234d3aa2a4542d6c6af8ded72fe93cece4
Adam Wali <adam.wali@square63.org>
2021-11-30 12:16:35 +0500

    Moods added to track serializer

--- app/serializers/api/v1/consumer/track_serializer.rb
+++ app/serializers/api/v1/consumer/track_serializer.rb
@@ -1,13 +1,17 @@
 class Api::V1::Consumer::TrackSerializer < ActiveModel::Serializer
 
   attributes :id, :title, :file, :status, :parent_track_id, :public_domain, :created_at, :lyrics, :explicit, :composer, :description,
-             :language, :instrumental, :key, :bpm, :admin_note, :filters, :genre, :duration
+             :language, :instrumental, :key, :bpm, :admin_note, :filters, :genre, :moods, :duration
 
   has_many :filters, serializer: Api::V1::FilterSerializer
   has_many :alternate_versions, serializer: Api::V1::TrackSerializer
 
   def genre
     object.filters.select { |filter| filter.parent_filter.name.downcase.include?('genres') }
+  end
+
+  def moods
+    object.filters.select { |filter| filter.parent_filter.name.downcase.include?('moods') }.pluck(:name)
   end
 
   def file
