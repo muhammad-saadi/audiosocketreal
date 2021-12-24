6558b3621f15ce21d80bf8a56e143b6ce89d52ea
Adam Wali <adam.wali@square63.org>
2021-12-24 11:21:39 +0500

    Feedback 2

--- app/models/contact_information.rb
+++ app/models/contact_information.rb
@@ -1,7 +1,7 @@
 class ContactInformation < ApplicationRecord
   belongs_to :artist_profile, touch: true
 
-  validates :name, :phone, :email, :street, :postal_code, :city, :state, :country, presence: true
+  validates :name, :phone, :email, :street, :postal_code, :state, :country, presence: true
   validates :email, email: true
   validates :phone, numericality: true
 end
